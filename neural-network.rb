4930267156b00b0b6a601673c95006be690c9f96
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-16 18:20:16 -0700

    Adds correct csv file

--- neural-network.rb
+++ neural-network.rb
@@ -1,4 +1,5 @@
-require 'ruby-fann' require 'csv'
+require 'ruby-fann'
+require 'csv'
 
 # Create two empty arrays. One will hold our independent varaibles (x_data), and the other will hold our dependent variable (y_data).
 x_data = []
@@ -24,3 +25,27 @@ y_test_data = y_data[0 .. (testing_size-1)]
 # Pick up where we left off until the end of the dataset.
 x_train_data = x_data[testing_size .. x_data.size]
 y_train_data = y_data[testing_size .. y_data.size]
+
+# Set up the training data model.
+train = RubyFann::TrainData.new(:inputs=> x_train_data, :desired_outputs=>y_train_data)
+
+# Set up the model and train using training data.
+model = RubyFann::Standard.new(
+              num_inputs: 2,
+              hidden_neurons: [6],
+              num_outputs: 1 );
+
+model.train_on_data(train, 1000, 10, 0.01)
+
+predicted = []
+
+# Iterate over our x_test_data, run our model on each one, and add it to our predicted array.
+x_test_data.each do |params|
+  predicted.push( model.run(params).map{ |e| e.round } )
+end
+
+# Compare the predicted results with the actual results.
+correct = predicted.collect.with_index { |e,i| (e == y_test_data[i]) ? 1 : 0 }.inject{ |sum,e| sum+e }
+
+# Print out the accuracy rate.
+puts "Accuracy: #{((correct.to_f / testing_size) * 100).round(2)}% - test set of size #{testing_percentage}%"
