2953cfdc7b9843f95b653fb7ca89a7397952c9be
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-24 11:24:13 -0700

    fixes syntax

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'ruby-fann'
+gem 'pry'
+gem 'pry-byebug'
--- neural-network.rb
+++ neural-network.rb
@@ -1,5 +1,6 @@
 require 'ruby-fann'
 require 'csv'
+require 'pry'
 
 # Create two empty arrays. One will hold our independent varaibles (x_data), and the other will hold our dependent variable (y_data).
 x_data = []
@@ -7,7 +8,7 @@ y_data = []
 
 # Iterate through our CSV data and add elements to applicable arrays.
 # Note that if we don't add the .to_f and .to_i, our arrays would have strings, and the ruby-fann library would not be happy.
-CSV.foreach("students.csv", headers: false) do |row|
+CSV.foreach("students-data.csv", headers: false) do |row|
   x_data.push([row[0].to_f, row[1].to_f])
   y_data.push(row[2].to_i)
 end
@@ -19,21 +20,23 @@ testing_percentage = 20.0
 testing_size = x_data.size * (testing_percentage/100.to_f)
 
 # Start at the beginning and end at the testing_size - 1 since arrays are 0-indexed.
-x_test_data = x_data[0 .. (testing_size-1)]
-y_test_data = y_data[0 .. (testing_size-1)]
+x_test_data = x_data[0...testing_size]
+y_test_data = y_data[0...testing_size]
 
 # Pick up where we left off until the end of the dataset.
-x_train_data = x_data[testing_size .. x_data.size]
-y_train_data = y_data[testing_size .. y_data.size]
+x_train_data = x_data[testing_size..x_data.size]
+y_train_data = y_data[testing_size..y_data.size]
 
 # Set up the training data model.
-train = RubyFann::TrainData.new(:inputs=> x_train_data, :desired_outputs=>y_train_data)
+binding.pry
+train = RubyFann::TrainData.new(inputs: x_train_data, desired_outputs: y_train_data)
 
 # Set up the model and train using training data.
 model = RubyFann::Standard.new(
-              num_inputs: 2,
-              hidden_neurons: [6],
-              num_outputs: 1 );
+  num_inputs: 2,
+  hidden_neurons: [6],
+  num_outputs: 1
+)
 
 model.train_on_data(train, 1000, 10, 0.01)
 
